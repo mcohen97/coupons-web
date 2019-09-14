37380d13dc48575bd1b21711aaf388e5627d9c61
= <diegomarcel27@hotmail.com>
2019-09-14 01:42:02 -0300

    se crea controller con operaciones basicas para promociones

--- db/migrate/20190912231150_create_promotions.rb
+++ db/migrate/20190912231150_create_promotions.rb
@@ -1,8 +1,12 @@
 class CreatePromotions < ActiveRecord::Migration[6.0]
   def change
     create_table :promotions do |t|
+      t.string :code
+      t.string :name
+      t.boolean :active
 
       t.timestamps
     end
+    add_index :promotions, :code, unique: true
   end
 end
