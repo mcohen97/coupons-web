21d28624a58cc18a7edadf768ad9a64006442676
Eusebio <eusebiodm98@gmail.com>
2019-09-24 14:11:37 -0300

    Added org to useR

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+
+    super
+  end
 
   # POST /resource
   # def create
@@ -38,25 +39,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    @org_name = params[:user][:organization]
+    @org = Organization.new(organization_name: @org_name)
+    @org.save
+    params[:user][:organization_id] = @org.id
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :organization_id])
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+# If you have extra params to permit, append them to the sanitizer.
+# def configure_account_update_params
+#   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+# end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+# The path used after sign up.
+# def after_sign_up_path_for(resource)
+#   super(resource)
+# end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+# The path used after sign up for inactive accounts.
+# def after_inactive_sign_up_path_for(resource)
+#   super(resource)
+# end
 end
