145f6c67acfc22e837b4aaab143f148d74075dd0
Eusebio <eusebiodm98@gmail.com>
2019-09-25 01:21:40 -0300

    Added tenants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
+  set_current_tenant_through_filter
+  before_action :set_current_user, :set_organization
+
+  def set_organization
+    if user_signed_in?
+      current_organization = Organization.find @current_user.organization_id
+      set_current_tenant(current_organization)
+    end
+  end
+
+  def user_signed_in?
+    @current_user
+  end
 
   private
 
