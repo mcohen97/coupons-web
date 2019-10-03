b95e32973fe9c848bda75960b11c9497e03f3166
= <diegomarcel27@hotmail.com>
2019-10-02 23:40:28 -0300

    Se cambia el nombre de coupon usage a coupon instance, el sistema de ahora en mas comenzara a dictar cuales son los cupones

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.4'
+ruby '2.6.3'
 
 gem 'rubocop', '~> 0.74.0', require: false
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/migrate/20190923210224_create_coupon_instances.rb
+++ db/migrate/20190923210224_create_coupon_instances.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class CreateCouponUsages < ActiveRecord::Migration[6.0]
+class CreateCouponInstances < ActiveRecord::Migration[6.0]
   def change
-    create_table :coupon_usages do |t|
+    create_table :coupon_instances do |t|
       t.string :coupon_code
       t.references :promotion, null: false, foreign_key: true
 
