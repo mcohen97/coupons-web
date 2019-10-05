35035ac2caf12115ff587926d3b0553cc45caf77
= <diegomarcel27@hotmail.com>
2019-10-05 14:28:18 -0300

    Se agrega logica de validacion de instancias de cupon

--- app/models/coupon_instance.rb
+++ app/models/coupon_instance.rb
@@ -4,4 +4,12 @@ class CouponInstance < ApplicationRecord
   belongs_to :promotion
   validates :coupon_code, presence: true
   validates_uniqueness_of :coupon_code
+  validate :belongs_to_coupon
+
+  def belongs_to_coupon
+    if promotion.type != 'Coupon'
+      errors.add(:base, 'Must belong to coupon')
+    end
+  end
+
 end
