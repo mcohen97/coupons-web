f88f03f59ff8fd65d8979502162b5d8c9a697ed2
Eusebio <eusebiodm98@gmail.com>
2019-10-05 16:03:54 -0300

    depl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.hosts << 'couponsarq3.azurewebsites.net'
+  config.hosts << ENV.fetch("HOSTS") { nil }
 
   # Settings specified here will take precedence over those in config/application.rb.
   raise 'JWT secret not set in enviroment' unless ENV['JWT_SECRET'].present?
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = ENV.fetch("ENABLE_ERRORS") { false }
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
