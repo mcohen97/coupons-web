636705a674a763e75f7681daf2b36ed5305be58c
= <diegomarcel27@hotmail.com>
2019-10-08 00:08:55 -0300

    Se atrapan excepciones de usuario no autorizado de forma centralizada y se renderiza transitoriamente la vista 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
+require_relative '../../lib/error/not_authorized_error.rb'
 
 class ApplicationController < ActionController::Base
 
+  rescue_from NotAuthorizedError, with: :not_authorized
   set_current_tenant_through_filter
   before_action :set_current_user, :set_organization
 
@@ -22,5 +24,13 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     @current_user = current_user if user_signed_in?
+  end
+
+  def not_authorized
+    respond_to do |format|
+      logger.error('User not authorized to perform that ac.')
+      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :forbidden }
+      format.json { render json: { error: 'Promotion not found.' }.to_json, status: :forbidden }
+    end
   end
 end
