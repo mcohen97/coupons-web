e26120d8fb9bf61eae6af7fbc7fe48663b38a8c9
= <diegomarcel27@hotmail.com>
2019-10-10 13:10:00 -0300

    Se agrega prueba de report desde el backoffice

--- test/controllers/promotions_controller_public_test.rb
+++ test/controllers/promotions_controller_public_test.rb
@@ -7,7 +7,7 @@ class PromotionsControllerPublicTest < ActionDispatch::IntegrationTest
   setup do
     @appkey = application_keys(:one)
     get '/users/sign_in'
-    sign_in user
+    sign_in users(:one)
     post user_session_url
   end
 
@@ -15,22 +15,13 @@ class PromotionsControllerPublicTest < ActionDispatch::IntegrationTest
     get generate_report_url(promotions(:coupon1)), headers: {Authorization: @appkey.generate_token }
     assert_response :success
     report = @controller.instance_variable_get(:@report)
-    assert_equal 7, report.invocations_count
-    assert_equal 4, report.negetive_responses
-    assert_equal  3/7, report.positive_ratio
-    assert_equal 4/7, report.negative_ratio
-    assert_equal 48, report.total_spent
+    assert_equal 7, report[:invocations_count]
+    assert_equal  3.0/7, report[:positive_ratio]
+    assert_equal 4.0/7, report[:negative_ratio]
+    assert_equal 48, report[:total_money_spent]
   end
 
-  test "should evaluate promotion correctly" do
-    puts @appkey.promotions.count
-    post evaluate_promotion_url, params: {	code: "COMIDADESC5",
-      attributes: {total: "12", products_size: 2, transaction_id:6}
-      }, 
-      headers: {Authorization: @appkey.generate_token, Content_Type: 'application/json' }
-    assert_response :success
-  end
-end
+
 =begin
   #CANT BUILD FIXTURES IN ORDER TO HAVE APPLICATION KEYS HAVE PROMOTIONS (MANY TO MANY RELATIONSHIP)
 
@@ -43,5 +34,5 @@ end
     assert_response :success
   end
   
-end
 =end
+end
