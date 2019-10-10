77adaed86c5ef8afc0f6b8753de580d9df66dbfe
= <diegomarcel27@hotmail.com>
2019-10-10 02:47:39 -0300

    Se agrega clase de pruebas para testear endpoints externos de promociones

---
+++ test/controllers/promotions_controller_public_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PromotionsControllerPublicTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @appkey = application_keys(:one)
+  end
+  
+end
