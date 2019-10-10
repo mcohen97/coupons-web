11f90b642bed2de9496aea9676203ca41610f5af
cantuccis <facuarancet97@gmail.com>
2019-10-10 04:31:41 -0300

    se mejora ui de reportes y se cambian rutas para que no aparezca el error del login inicial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :authenticate_user!
 
-  def index; end
+  def index
+    unless user_signed_in?
+      redirect_to new_user_session_path and return
+    end
+    unless helpers.is_current_user_admin
+      redirect_to promotions_path
+    end
+  end
 
   def invitation
-
+    unless user_signed_in?
+      redirect_to new_user_session_path and return
+    end
   end
 
   def invite
+    unless user_signed_in?
+      redirect_to new_user_session_path and return
+    end
     invited_email = params[:email]
     invitation = EmailInvitation.create(user: current_user, invited_email: invited_email, organization_id: current_user.organization_id) 
     UserMailer.with(email_invited: invited_email, sender: current_user, organization_name: current_user.organization, invitation: invitation).invitation_email.deliver_later
@@ -17,4 +28,5 @@ class HomeController < ApplicationController
     redirect_to home_path
     logger.info("Email sent, from #{current_user.email}, to #{invited_email}")
   end
+  
 end
