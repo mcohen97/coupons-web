4761060a03740d31d746f83304639ec74eb74c37
= <diegomarcel27@hotmail.com>
2019-11-24 14:56:29 -0300

    Se crea servicio de usuarios con el login implementado

---
+++ app/services/users_service.rb
@@ -0,0 +1,73 @@
+require_relative '../../lib/error/service_response_error.rb'
+
+
+class UsersService
+
+#temporarily against microservice
+GATEWAY_URL = 'https://coupons-gateway.herokuapp.com'
+
+def self.instance()
+  @instance = @instance || UsersService.new()
+  return @instance
+end
+
+def send_invitation(authorization)
+end
+
+def sign_in(email, password)
+  route = '/v1/signin/'+ email 
+
+  post route, {password: password}, ''
+end
+
+def create_user(invitation_code, payload)
+end
+
+private
+
+  def initialize
+    @connection = create_connection
+  end
+  
+  def create_connection
+    
+    conn = Faraday.new(url: GATEWAY_URL) do |c|
+      c.response :logger
+      c.request :json
+      c.use Faraday::Adapter::NetHttp
+    end
+
+    return conn
+  end
+
+  def get (url, authorization)
+    resp = @connection.get url do |request|
+      request.headers["Authorization"] = authorization
+      request.headers['Content-Type'] = 'application/json'
+    end
+
+    handle_response(resp)
+
+  end
+
+  def post(url, payload, authorization)
+    print('se va a invocar el url')
+
+    resp = @connection.post url do |request|
+      request.headers["Authorization"] = authorization
+      request.headers['Content-Type'] = 'application/json'
+      request.body = payload.to_json
+    end
+
+    handle_response(resp)
+  end
+
+  def handle_response(response)
+    if response.success?
+      return JSON.parse response.body
+    else
+      raise ServiceResponseError
+    end
+  end
+
+end
