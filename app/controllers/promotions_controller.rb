c1cd8b8de67a192300400f12ff60884c22a4d966
cantuccis <facuarancet97@gmail.com>
2019-11-26 17:25:10 -0300

    Avance en reportes

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -85,11 +85,28 @@ class PromotionsController < ApplicationController
   
 
   def report
-    if is_backoffice_client
-      respond_to_backoffice
+    promotion_id = params[:id]
+
+    @promotion = PromotionsService.instance.get_promotion_by_id(promotion_id)
+
+    demographic_response = ReportsService.instance.get_demographic_report(promotion_id)
+    if demographic_response.success
+      @demographic_report = demographic_response.data
     else
-      respond_to_external
+      flash[:error] = demographic_response.data
     end
+    
+    usage_response = ReportsService.instance.get_usage_report(promotion_id)
+    if usage_response.success
+      @usage_report = usage_response.data
+    else
+      flash[:error] = usage_response.data
+    end
+
+    respond_to do |format|
+      format.html { :report }
+    end
+
   end
 
   private
---
+++ app/services/reports_service.rb
@@ -0,0 +1,29 @@
+class ReportsService
+    include HttpRequests
+  
+  #temporarily against microservice
+  GATEWAY_URL = 'https://coupons-reports.herokuapp.com'
+  
+  
+  def self.instance()
+    @instance = @instance || ReportsService.new()
+    return @instance
+  end
+  
+  def get_demographic_report(promotion_id)
+    route = 'api/v1/reports/demographic/'+promotion_id
+    get route
+  end
+  
+  def get_usage_report(promotion_id)
+    route = 'api/v1/reports/usage/'+promotion_id
+    get route
+  end
+  
+  private
+  
+    def initialize
+      @connection = create_connection(GATEWAY_URL)
+    end
+    
+  end
